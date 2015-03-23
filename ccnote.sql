987f7dbb8b41039e70c54234a2ca792fd234158b
tajpure <tajpure@gmail.com>
2015-03-23 18:58:06 +0800

    sql

--- ccnote.sql
+++ ccnote.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.21, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ccnote
 -- ------------------------------------------------------
--- Server version	5.5.41-0ubuntu0.14.04.1
+-- Server version	5.6.21-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,7 +28,7 @@ CREATE TABLE `note` (
   `content` longtext,
   `user_name` varchar(45) NOT NULL,
   PRIMARY KEY (`nid`)
-) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,29 @@ LOCK TABLES `note` WRITE;
 /*!40000 ALTER TABLE `note` DISABLE KEYS */;
 INSERT INTO `note` VALUES (67,'node.js','Node.js is an open source, cross-platform runtime environment for server-side and networking applications. Node.js applications are written in JavaScript, and can be run within the Node.js runtime on OS X, Microsoft Windows, Linux and FreeBSD.\n\nNode.js provides an event-driven architecture and a non-blocking I/O API that optimizes an application\'s throughput and scalability. These technologies are commonly used for real-time applications.\n\nNode.js uses the Google V8 JavaScript engine to execute code, and a large percentage of the basic modules are written in JavaScript. Node.js contains a built-in library to allow applications to act as a Web server without software such as Apache HTTP Server or IIS.','taojx'),(69,'Scheme','Scheme is a functional programming language and one of the two main dialects of the programming language Lisp. Unlike Common Lisp, the other main dialect, Scheme follows a minimalist design philosophy specifying a small standard core with powerful tools for language extension.\n\nScheme was developed at the MIT AI Lab by Guy L. Steele and Gerald Jay Sussman who introduced it to the academic world via a series of memos, now referred to as the Lambda Papers, over the period 1975–1980. It was the first dialect of Lisp to choose lexical scope and the first to require implementations to perform tail-call optimization, giving stronger support for functional programming and associated techniques such as recursive algorithms. It was also one of the first programming languages to support first-class continuations. It had a significant influence on the effort that led to the development of Common Lisp.','taojx'),(70,'LLVM','The LLVM Project is a collection of modular and reusable compiler and toolchain technologies. Despite its name, LLVM has little to do with traditional virtual machines, though it does provide helpful libraries that can be used to build them. The name \"LLVM\" itself is not an acronym; it is the full name of the project.','taojx'),(71,'Docker','Docker is an open platform for developers and sysadmins to build, ship, and run distributed applications. Consisting of Docker Engine, a portable, lightweight runtime and packaging tool, and Docker Hub, a cloud service for sharing applications and automating workflows, Docker enables apps to be quickly assembled from components and eliminates the friction between development, QA, and production environments. As a result, IT can ship faster and run the same app, unchanged, on laptops, data center VMs, and any cloud.','taojx'),(72,'C','In computing, C (/?si?/, as in the letter C) is a general-purpose programming language initially developed by Dennis Ritchie between 1969 and 1973 at AT&T Bell Labs.[5][6] Like most imperative languages in the ALGOL tradition, C has facilities for structured programming and allows lexical variable scope and recursion, while a static type system prevents many unintended operations. Its design provides constructs that map efficiently to typical machine instructions, and therefore it has found lasting use in applications that had formerly been coded in assembly language, most notably system software like the Unix computer operating system.[7]\n\nC is one of the most widely used programming languages of all time,[8][9] and C compilers are available for the majority of available computer architectures and operating systems.','tajpure'),(73,'Java','Java is a general-purpose computer programming language that is concurrent, class-based, object-oriented,[10] and specifically designed to have as few implementation dependencies as possible. It is intended to let application developers \"write once, run anywhere\" (WORA),[11] meaning that code that runs on one platform does not need to be recompiled to run on another.[12] Java applications are typically compiled to bytecode that can run on any Java virtual machine (JVM) regardless of computer architecture. Java is, as of 2014, one of the most popular programming languages in use, particularly for client-server web applications, with a reported 9 million developers.[13][14] Java was originally developed by James Gosling at Sun Microsystems (which has since merged into Oracle Corporation) and released in 1995 as a core component of Sun Microsystems\' Java platform. The language derives much of its syntax from C and C++, but it has fewer low-level facilities than either of them.','tajpure'),(74,'linux','Linux (Listeni/?l?n?ks/ lin-uks[5][6] or, less frequently used, /?la?n?ks/ lyn-uks)[6][7] is a Unix-like and mostly POSIX-compliant[8] computer operating system assembled under the model of free and open-source software development and distribution. The defining component of Linux is the Linux kernel,[9] an operating system kernel first released on 5 October 1991 by Linus Torvalds.[10][11] The Free Software Foundation uses the name GNU/Linux, which has led to some controversy.[12][13]\n\nLinux was originally developed as a free operating system for Intel x86-based personal computers. It has since been ported to more computer hardware platforms than any other operating system.[citation needed] It is a leading operating system on servers and other big iron systems such as mainframe computers and supercomputers.[14][15][16] As of November 2014, 97% of the world\'s 500 fastest supercomputers run some variant of Linux,[17] including the top 80.[18] Linux also runs on embedded systems, which are devices whose operating system is typically built into the firmware and is highly tailored to the system; this includes mobile phones,[19] tablet computers, network routers, facility automation controls, televisions[20][21] and video game consoles. Android, the most widely used operating system for tablets and smartphones, is built on top of the Linux kernel.[22]','tajpure'),(75,'','nserted data has been truncated as the control\'s limit was reached. Please use the value editor instead for editing such large text data.',''),(79,'订单','顶顶顶顶','tajpure');
 /*!40000 ALTER TABLE `note` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shared_note`
+--
+
+DROP TABLE IF EXISTS `shared_note`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shared_note` (
+  `username` varchar(20) NOT NULL,
+  `nid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shared_note`
+--
+
+LOCK TABLES `shared_note` WRITE;
+/*!40000 ALTER TABLE `shared_note` DISABLE KEYS */;
+INSERT INTO `shared_note` VALUES ('tajpure',70),('tajpure',69),('tajpure',67),('taojx',69);
+/*!40000 ALTER TABLE `shared_note` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -75,4 +98,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-03-21 18:31:51
+-- Dump completed on 2015-03-23 18:57:35
